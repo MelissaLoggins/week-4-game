469cc9b6326ec3f894aa5db4fe232f0386ba8bd4
MelissaLoggins <melissaloggins@gmail.com>
2016-12-11 14:16:56 -0500

    omg, i think i got it working and wanted to save it

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -2,27 +2,36 @@
 $(document).ready(function(){
 
 //Variables================================================================
-
-var 
+ 
 
         // When random-button is clicked...
       $("#gem1").on("click", function() {
-          alert("test");
-        // ...create a string which will hold the lottery number.
-        var lottoNumber = "";
+        
+        var randomNumber="";
 
-        // Then create a loop that generates 9 separate numbers.
-        for (var i = 0; i < 9; i++) {
-          var random = Math.floor(Math.random() * 9) + 1;
+        for (var i = 0; i < 2; i++) {
+          var random = Math.floor(Math.random() * 2) + 1;
 
-          // Add to the lottery number.
-          lottoNumber = random + lottoNumber;
+          randomNumber = random + randomNumber; 
         }
+        console.log(randomNumber)
+      })
+    })
+    //     // ...create a string which will hold the lottery number.
+    //     var lottoNumber = "";
 
-        // Then prepend the lotto number to the top of our random-number div.
-        $("#gem1").prepend("<br><hr>" + lottoNumber);
-      });
-    });
+    //     // Then create a loop that generates 9 separate numbers.
+    //     for (var i = 0; i < 9; i++) {
+    //       var random = Math.floor(Math.random() * 9) + 1;
+
+    //       // Add to the lottery number.
+    //       lottoNumber = random + lottoNumber;
+    //     }
+
+    //     // Then prepend the lotto number to the top of our random-number div.
+    //     $("#gem1").prepend("<br><hr>" + lottoNumber);
+    //   });
+    // });
 
 
 //Set-up gems===============================================================
