3a7bc03772a8afd7dead593ec04e73cd07f8659f
MelissaLoggins <melissaloggins@gmail.com>
2017-03-02 11:18:05 -0500

    changes in javascript and in style. not complete

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -10,134 +10,87 @@ var gem4
 var score
 var wins=0;
 var losses=0; 
+var winDisplay = $("#wins");
+var lossDisplay = $("#losses");
 
-// Gem set-up~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Game set-up~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//We set goal equal to a random number between 19 and 120. 
+//We set the score equal to 0. 
+//We display the score and the goal on the page using jquery. 
 function setUp(){
   goal = Math.floor(Math.random()*120+19);
-  gem1 = Math.floor(Math.random()*12+1);
-  gem2 = Math.floor(Math.random()*12+1);
-  gem3 = Math.floor(Math.random()*12+1);
-  gem4 = Math.floor(Math.random()*12+1);
   score=0;
-  $('#goal').html(goal)
-  console.log("Gem1: "+gem1);
-  console.log("Gem2: "+gem2);
-  console.log("Gem3: "+gem3);
-  console.log("Gem4: "+gem4);
+  $('#score').html(score);
+  $('#goal').html(goal);
 };
-
-// Click results~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  function outcome() {
-    if(score===goal){
-      $('outcome').html("Lookatcha!");
-      winner.play();
-      wins++;
-      $('#score').empty();
-      setUp();
-    }
-    else if(score>goal) {
-      $('.outcome').html("Loser!");
-      loser.play();
-      losses++;
-      $('#score').empty();
-      setUp();
-    }
-
-    $('#wins').html("Wins: " + wins);
-    $('#losses').html("Losses: "+losses);
+//This function testWin() is used to test the win/lose situation for the user. 
+//Our first if takes into account whether or not the user has their score equal to the goal.
+//If they do, we know that the user has won, so we increment the wins variable by 1. 
+//Since they have won, we want to display the wins on the page. 
+//We use jQuery to display this on the page via the winDisplay.html(wins).
+//We use else if here, because we have to account for every possible scenario, this includes
+//Winning, losing and just continue playing. The else if allows us to continue play.
+//Lastly, the else covers, in this case, our only remaining scenario in which the player has 
+//a score that is higher than the goal, in which case they have lost. 
+//Each win/lose statement also calls setUp() to restart the game.
+function testWin(){
+  if(score === goal){
+    wins++; 
+    winDisplay.html(wins);
+    setUp(); 
   }
-
-
-
-
-
-//Variables================================================================
- // var min = 19;
- // var max = 120;
- // var gemValue = 0;
-
- // Variables for counter displays*****************************************
-// var winDisplay = $("#wins");
-// var lossDisplay = $("#losses");
-// var userScoreDisplay = $("#userTurn");
-// var guruNumber = $("#guruHead");
-
-
-// Variables of random number and of the user score~~~~~~~~~~~~~~~~~~~~~~~~
-// var wins = 0;
-// var losses = 0;
-// var userTurn = 0;
-
-// function getRandomNumber(a, b) {
-//   guruhead = Math.floor(Math.random() * (b-a) + a);
-//   $("#guruNumber").html(guruHead);
-//   console.log(guruHead);
-// }
+  else if(score < goal){
+    console.log("Keep going!");
+  }
+  else{
+    losses++;
+    lossDisplay.html(losses);
+    setUp();
+  }
+}
 
 // Gem setup===============================================================
-        // When random-button is clicked...
-       // $("#gem1").on("click", function() {
-        
-       //   var randomNumber="";
+        // Each gem below is assigned a variable with the gem's name.
+        // Then it is setting that variable equal to a random number between 1 - 9
+        // Then we add that gems value to the user's score, since this code
+        // will only fire off once we are clicking on the ID in question. 
+       $("#gem1").on("click", function() {
+           gem1 = Math.floor(Math.random() * 9) + 1;
+           score += gem1; 
+           console.log("Gem value: "+gem1);
+           console.log("User's score is: "+score);
+           $('#score').html(score);
+           testWin(); 
+       });
 
-       //   for (var i = 0; i < 2; i++) {
-       //     var random = Math.floor(Math.random() * 9) + 1;
+       $("#gem2").on("click", function() {
+           gem2 = Math.floor(Math.random() * 9) + 1;
+           score += gem2; 
+           console.log("Gem value: "+gem2);
+           console.log("User's score is: "+score);
+           $('#score').html(score);
+           testWin(); 
+       });
 
-       //    randomNumber = random + randomNumber; 
-       //   }
-       //   console.log(randomNumber)
-       // })
+       $("#gem3").on("click", function() {
+           gem3 = Math.floor(Math.random() * 9) + 1;
+           score += gem3; 
+           console.log("Gem value: "+gem3);
+           console.log("User's score is: "+score);
+           $('#score').html(score);
+           testWin(); 
+        })
 
-
-       // $("#gem2").on("click", function() {
-        
-       //   var randomNumber="";
-
-       //   for (var i = 0; i < 2; i++) {
-       //     var random = Math.floor(Math.random() * 9) + 1;
-
-       //     randomNumber = random + randomNumber; 
-       //   }
-       //  console.log(randomNumber)
-       // })
-
-       // $("#gem3").on("click", function() {
-        
-   // ...create a string which will hold the random gem number.
-         // var randomNumber="";
-
-// // Then create a loop that generates 2(?) separate numbers.
-         // for (var i = 0; i < 2; i++) {
-         //   var random = Math.floor(Math.random() * 9) + 1;
-
-// // Add to the randomNumber number.
-       //     randomNumber = random + randomNumber; 
-       //   }
-       //   console.log(randomNumber)
-       // })
-
-       // $("#gem4").on("click", function() {
-        
-       //   var randomNumber="";
-
-       //   for (var i = 0; i < 2; i++) {
-       //     var random = Math.floor(Math.random() * 9) + 1;
-
-       //     randomNumber = random + randomNumber; 
-       //   }
-       //   console.log(randomNumber)
-       //  })
-
-        
-    
-       // }
- 
-
+       $("#gem4").on("click", function() {
+          gem4 = Math.floor(Math.random() * 9) + 1;
+          score += gem4; 
+          console.log("Gem value: "+gem4);
+          console.log("User's score is: "+score);
+          $('#score').html(score);
+          testWin(); 
+        });
 //Set-up and play the game=================================================
 setUp();
-playGame();
-console.log("Goal: "+goal);
-
 });
-	
+  
 
